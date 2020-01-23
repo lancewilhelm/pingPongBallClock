c42501f97fe8f4cc74730b50a3d32ec2f5778196
Lance Wilhelm <lance.wilhelm@gmail.com>
2020-01-22 21:38:05 -0700

    adding all of the pixels for test

--- code/test.py
+++ code/test.py
@@ -2,7 +2,7 @@ from neopixel import *
 
 # LED strip configuration:
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-LED_COUNT      = 4
+LED_COUNT      = 256
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10       # DMA channel to use for generating signal (try 5)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
@@ -13,5 +13,7 @@ LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
 strip.begin()
 
-strip.setPixelColor(0, Color(255,0,0))
+for i in range(self.strip.numPixels()):
+    strip.setPixelColor(i, Color(255,0,0))
 strip.show()
+
