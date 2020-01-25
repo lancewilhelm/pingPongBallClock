7a8e21a5704bc968ef0ee18d80497670af05a9f9
Lance Wilhelm <lance.wilhelm@gmail.com>
2020-01-24 23:29:28 -0700

    removed try catch

--- code/main.py
+++ code/main.py
@@ -26,65 +26,62 @@ def clock():
     global secsPrev
     global colonLit
 
-    try:
-        while(True):
-            # Write the BG. Will not overwrite text per the function
-            LED.colorFill(LED.bgColor)
-            # LED.rainbow()
-            # LED.rainbowCycle()
+    while(True):
+        # Write the BG. Will not overwrite text per the function
+        LED.colorFill(LED.bgColor)
+        # LED.rainbow()
+        # LED.rainbowCycle()
 
-            # Get the current local time and parse it out to usable variables
-            t = time.localtime()
-            hours = t.tm_hour
-            mins = t.tm_min
-            secs = t.tm_sec
+        # Get the current local time and parse it out to usable variables
+        t = time.localtime()
+        hours = t.tm_hour
+        mins = t.tm_min
+        secs = t.tm_sec
 
-            # Convert 24h time to 12h time
-            if hours > 12:
-                hours -= 12
+        # Convert 24h time to 12h time
+        if hours > 12:
+            hours -= 12
 
-            # Check to see if the second has changed. If it has, changed the colonLit activation
-            if secs != secsPrev:
-                colonLit ^= True  #flip the colonLit bool
+        # Check to see if the second has changed. If it has, changed the colonLit activation
+        if secs != secsPrev:
+            colonLit ^= True  #flip the colonLit bool
 
-                if colonLit:
-                    LED.writeBall(8,4,LED.textColor,True)
-                    LED.writeBall(8,2,LED.textColor,True)
-                    LED.strip.show()
-                else: 
-                    LED.writeBall(8,4,LED.textColor,False)     #Keep the color white, but we toggle the text to False so that it will be overwritten by the rainbow
-                    LED.writeBall(8,2,LED.textColor,False)     #Keep the color white, but we toggle the text to False so that it will be overwritten by the rainbow
-                    LED.strip.show()
+            if colonLit:
+                LED.writeBall(8,4,LED.textColor,True)
+                LED.writeBall(8,2,LED.textColor,True)
+                LED.strip.show()
+            else: 
+                LED.writeBall(8,4,LED.textColor,False)     #Keep the color white, but we toggle the text to False so that it will be overwritten by the rainbow
+                LED.writeBall(8,2,LED.textColor,False)     #Keep the color white, but we toggle the text to False so that it will be overwritten by the rainbow
+                LED.strip.show()
 
-                secsPrev = secs
+            secsPrev = secs
 
-            # Check to see if the minute has changed. If it has, write the new minute
-            if mins != minsPrev:    
-                # Convert the mins to a string so that we can parse the individual numbers for display
-                minsStr = str(mins)
+        # Check to see if the minute has changed. If it has, write the new minute
+        if mins != minsPrev:    
+            # Convert the mins to a string so that we can parse the individual numbers for display
+            minsStr = str(mins)
 
-                # Write the actual numerals
-                if mins < 10:
-                    LED.writeChar(10,1,0,LED.textColor)
-                    LED.writeChar(14,1,int(minsStr[0]),LED.textColor)
-                else:
-                    LED.writeChar(10,1,int(minsStr[0]),LED.textColor)
-                    LED.writeChar(14,1,int(minsStr[1]),LED.textColor)
-                minsPrev = mins
+            # Write the actual numerals
+            if mins < 10:
+                LED.writeChar(10,1,0,LED.textColor)
+                LED.writeChar(14,1,int(minsStr[0]),LED.textColor)
+            else:
+                LED.writeChar(10,1,int(minsStr[0]),LED.textColor)
+                LED.writeChar(14,1,int(minsStr[1]),LED.textColor)
+            minsPrev = mins
 
-            # Check to see if the hour has changed. If it has, write the new hour
-            if hours != hoursPrev:
-                # Convert the mins to a string so that we can parse the individual numbers for display
-                hoursStr = str(hours)
+        # Check to see if the hour has changed. If it has, write the new hour
+        if hours != hoursPrev:
+            # Convert the mins to a string so that we can parse the individual numbers for display
+            hoursStr = str(hours)
 
-                if hours < 10:
-                    LED.writeChar(4,1,int(hoursStr[0]),LED.textColor)
-                else:
-                    LED.writeChar(0,1,int(hoursStr[0]),LED.textColor)
-                    LED.writeChar(4,1,int(hoursStr[1]),LED.textColor)
-                hoursPrev = hours
-        except KeyboardInterrupt:
-            sys.exit()
+            if hours < 10:
+                LED.writeChar(4,1,int(hoursStr[0]),LED.textColor)
+            else:
+                LED.writeChar(0,1,int(hoursStr[0]),LED.textColor)
+                LED.writeChar(4,1,int(hoursStr[1]),LED.textColor)
+            hoursPrev = hours
 
 if __name__ == '__main__':
     x = threading.Thread(target=clock, args=())
