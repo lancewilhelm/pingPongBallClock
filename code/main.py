1e1359a0779073c26eeb1927f73f9cac7c050089
Lance Wilhelm <lance.wilhelm@gmail.com>
2020-01-26 14:47:17 -0700

    trying debug=false for flask

--- code/main.py
+++ code/main.py
@@ -8,7 +8,7 @@ from neopixel import *
 
 if __name__ == '__main__':
 	# Start the flask server
-	x = threading.Thread(target=app.run, kwargs=dict(use_reloader=False))
+	x = threading.Thread(target=app.run, kwargs=dict(debug=False, use_reloader=False))
 	x.daemon = True
 	x.start()
 	
