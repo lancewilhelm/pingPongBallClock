7daa2bb9284688c4f6d99fbcd8d6f9712a86020a
Lance Wilhelm <lance.wilhelm@gmail.com>
2020-01-25 23:38:59 -0700

    think I got it...

--- code/LEDUtil.py
+++ code/LEDUtil.py
@@ -104,24 +104,24 @@ class LEDStrip:
     def wheel(self,pos):
         # Generate rainbow colors across 0-255 positions.
         if pos < 85:
-            return Color(pos * 3, 255 - pos * 3, 0)
+            return Color(pos * 3, 255 - pos * 3, 0)     #green to red
         elif pos < 170:
             pos -= 85
-            return Color(255 - pos * 3, 0, pos * 3)
+            return Color(255 - pos * 3, 0, pos * 3)     #red to blue
         else:
             pos -= 170
-            return Color(0, pos * 3, 255 - pos * 3)
+            return Color(0, pos * 3, 255 - pos * 3)     #blue to green
 
     def wheelOpp(self,pos):
         # Generate rainbow colors across 0-255 positions.
         if pos < 85:
-            return Color(pos * 3, 0 + pos * 3, 0)
+            return Color(255 - pos * 3, pos * 3, 0)
         elif pos < 170:
             pos -= 85
-            return Color(0 + pos * 3, 0, pos * 3)
+            return Color(pos * 3, 0, 255 - pos * 3)
         else:
             pos -= 170
-            return Color(0, pos * 3, 0 + pos * 3)
+            return Color(0, 255 - pos * 3, pos * 3)
 
     def rainbow(self,wait_ms=20):
         # Draw rainbow that fades across all pixels at once.
