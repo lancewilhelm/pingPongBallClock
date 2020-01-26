2265aa1e2687bcccac50bc4b538c5f99283fa9bd
Lance Wilhelm <lance.wilhelm@gmail.com>
2020-01-26 14:50:49 -0700

    changing port to 80, removing debugger shit

--- code/main.py
+++ code/main.py
@@ -8,7 +8,7 @@ from neopixel import *
 
 if __name__ == '__main__':
 	# Start the flask server
-	x = threading.Thread(target=app.run, kwargs=dict(host='0.0.0.0',debug=False,use_reloader=False))
+	x = threading.Thread(target=app.run, kwargs=dict(host='0.0.0.0',port=80))
 	x.daemon = True
 	x.start()
 	
