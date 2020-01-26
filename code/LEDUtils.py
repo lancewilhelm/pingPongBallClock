1066cfe1ac0f51ffbf41376aa8c23dbf2a714dad
Lance Wilhelm <lance.wilhelm@gmail.com>
2020-01-26 14:19:05 -0700

    removed wheelOpp

--- code/LEDUtils.py
+++ code/LEDUtils.py
@@ -119,17 +119,6 @@ class PingPongBoard:
 			pos -= 170
 			return Color(0, pos * 3, 255 - pos * 3)     #blue to green
 
-	def wheelOpp(self,pos):
-		# Generate rainbow colors across 0-255 positions.
-		if pos < 85:
-			return Color(255 - pos * 3, pos * 3, 0)
-		elif pos < 170:
-			pos -= 85
-			return Color(pos * 3, 0, 255 - pos * 3)
-		else:
-			pos -= 170
-			return Color(0, 255 - pos * 3, pos * 3)
-
 	def rainbow(self,wait_ms=20):
 		# Draw rainbow that fades across all pixels at once.
 		j = self.updateFrame(256)
