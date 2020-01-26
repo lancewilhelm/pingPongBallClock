cedf5f287f147bd89aab37578ed18315934dae61
Lance Wilhelm <lance.wilhelm@gmail.com>
2020-01-26 14:44:49 -0700

    trying the flask daemon thread again

--- code/main.py
+++ code/main.py
@@ -8,12 +8,8 @@ from neopixel import *
 
 if __name__ == '__main__':
 	# Start the flask server
-	x = threading.Thread(target=app.run, args=())
-	x.daemon = False
+	x = threading.Thread(target=app.run, kwargs=dict(use_reloader=False))
+	x.daemon = True
 	x.start()
 	
-	# Start the clock server
-	y = threading.Thread(target=PPB.clock, args=())
-	y.daemon = True
-	y.start()
-	# PPB.clock()
+	PPB.clock()
