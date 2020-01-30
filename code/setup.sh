c4fe15f4f08917c2c54db7a4128cfd0e7c1e6b9c
Lance Wilhelm <lance.wilhelm@gmail.com>
2020-01-29 18:16:28 -0700

    setup.sh should be correct now

--- code/setup.sh
+++ code/setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #let's get neopixel set up
 sudo apt-get update
-sudo apt-get install build-essential python-dev git scons swig
+
+#let's get flask installed
+sudo apt-get install python-pip
+sudo pip install flask
+sudo pip install requests
+
+sudo apt-get install scons wig
 git clone https://github.com/jgarff/rpi_ws281x.git
 cd rpi_ws281x
 scons
 cd python
 sudo python setup.py install
-
-#let's get flask installed
-sudo apt-get install python-pip
-sudo pip install flask
