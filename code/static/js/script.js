7f1a68e8a64c81e375a1e29536520020539a76eb
Lance Wilhelm <lance.wilhelm@gmail.com>
2020-02-18 13:40:51 -0700

    trying setTimeout to make sure getconfigslist works

--- code/static/js/script.js
+++ code/static/js/script.js
@@ -319,7 +319,7 @@ function updateConfiguration(action) {
 
     // Close the modal and get the new list of configs from the server
     document.getElementById("customConfigModal").style.display = "none";
-    getConfigsList();
+    setTimeout(() => {getConfigsList();}, 500);
 }
 
 function getConfigsList() {
